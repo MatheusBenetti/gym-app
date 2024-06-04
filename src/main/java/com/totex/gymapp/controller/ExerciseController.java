41d61f87026f8ea1b30ebcb226c36168efe06566
MatheusBenetti <mbbenetti1@gmail.com>
2024-06-04 18:56:23 -0300

    create post method in ExerciseController

---
+++ src/main/java/com/totex/gymapp/controller/ExerciseController.java
@@ -0,0 +1,25 @@
+package com.totex.gymapp.controller;
+
+import com.totex.gymapp.controller.dto.CreateExerciseDto;
+import com.totex.gymapp.entity.Exercise;
+import com.totex.gymapp.service.ExerciseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExerciseController {
+
+    private final ExerciseService exerciseService;
+
+    public ExerciseController(ExerciseService exerciseService) {
+        this.exerciseService = exerciseService;
+    }
+
+    @PostMapping("/exercise")
+    public ResponseEntity<Exercise> createExercise(@RequestBody CreateExerciseDto dto) {
+        var exercise = exerciseService.createExercise(dto);
+        return ResponseEntity.ok(exercise);
+    }
+}

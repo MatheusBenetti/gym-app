387563806bd64b1ada35cfe179aaff3b8e887749
MatheusBenetti <mbbenetti1@gmail.com>
2024-06-05 19:25:37 -0300

    add findExerciseByExerciseName to exercise repository

--- src/main/java/com/totex/gymapp/repository/ExerciseRepository.java
+++ src/main/java/com/totex/gymapp/repository/ExerciseRepository.java
@@ -3,5 +3,8 @@ package com.totex.gymapp.repository;
 import com.totex.gymapp.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+    Optional<Exercise> findExerciseByExerciseName(String exerciseName);
 }

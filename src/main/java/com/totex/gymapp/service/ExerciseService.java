9dcc65180cceefd33f509b4c67c8929527fb3789
MatheusBenetti <mbbenetti1@gmail.com>
2024-06-05 19:26:03 -0300

    add validation to exercise controller

--- src/main/java/com/totex/gymapp/service/ExerciseService.java
+++ src/main/java/com/totex/gymapp/service/ExerciseService.java
@@ -2,6 +2,7 @@ package com.totex.gymapp.service;
 
 import com.totex.gymapp.controller.dto.CreateExerciseDto;
 import com.totex.gymapp.entity.Exercise;
+import com.totex.gymapp.exception.ExerciseExistsException;
 import com.totex.gymapp.repository.ExerciseRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,12 @@ public class ExerciseService {
     }
 
     public Exercise createExercise(CreateExerciseDto dto) {
+        var exercise = exerciseRepository.findExerciseByExerciseName(dto.exerciseName());
+
+        if (exercise.isPresent()) {
+            throw new ExerciseExistsException("Exercise already exists");
+        }
+
         return exerciseRepository.save(dto.toExercise());
     }
 }

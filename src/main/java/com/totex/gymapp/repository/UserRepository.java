542e174d24dc780b3fe5145f77553f0103019d12
MatheusBenetti <mbbenetti1@gmail.com>
2024-06-06 19:16:37 -0300

    create find user in user repository

--- src/main/java/com/totex/gymapp/repository/UserRepository.java
+++ src/main/java/com/totex/gymapp/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.totex.gymapp.repository;
 import com.totex.gymapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByFullName(String fullName);
 }

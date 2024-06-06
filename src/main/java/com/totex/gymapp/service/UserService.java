6643b1d0a7395059909282a06b40cd33220df0a6
MatheusBenetti <mbbenetti1@gmail.com>
2024-06-06 19:16:06 -0300

    create user service

---
+++ src/main/java/com/totex/gymapp/service/UserService.java
@@ -0,0 +1,24 @@
+package com.totex.gymapp.service;
+
+import com.totex.gymapp.controller.dto.CreateUserDto;
+import com.totex.gymapp.entity.User;
+import com.totex.gymapp.repository.UserRepository;
+
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(CreateUserDto dto) {
+        var user = userRepository.findUserByFullName(dto.fullName());
+
+        if (user.isPresent()) {
+            // TODO: create new exception class
+            throw new RuntimeException("User already exists");
+        }
+
+        return userRepository.save(dto.toUser());
+    }
+}

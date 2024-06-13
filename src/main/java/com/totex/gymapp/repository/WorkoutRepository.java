3222e85050d255b5fe3adf0219d627a421b11e27
MatheusBenetti <mbbenetti1@gmail.com>
2024-06-13 19:03:37 -0300

    add findWorkoutByName to WorkoutRepository

--- src/main/java/com/totex/gymapp/repository/WorkoutRepository.java
+++ src/main/java/com/totex/gymapp/repository/WorkoutRepository.java
@@ -3,5 +3,8 @@ package com.totex.gymapp.repository;
 import com.totex.gymapp.entity.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+    Optional<Workout> findWorkoutByName(String name);
 }
